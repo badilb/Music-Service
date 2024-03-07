4c5d08cd0e302d58d29046fba185b4da16368af0
baattezu <147482395+baattezu@users.noreply.github.com>
2024-03-07 12:01:05 +0600

    Remove unnecessary frontend files

--- internal/rest/dto/dto.go
+++ internal/rest/dto/dto.go
@@ -1,8 +1,8 @@
 package dto
 
 type LoginForm struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"user"`
+	Password string `json:"pwd"`
 }
 
 type UpdateForm struct {
